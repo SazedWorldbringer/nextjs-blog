8c9f9fe65297675fbd79490fd1aa51e80039919b
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-29 13:56:53 +0530

    feat: update site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Atharva Pardeshi";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "atharvaRants | Atharva Pardeshi";
 
 export default function Layout({ children, home }) {
   return (
