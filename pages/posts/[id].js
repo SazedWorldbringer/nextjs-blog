425cbc8d63e72996ce65403a093885313f3a6678
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-04 23:54:25 +0530

    feat: import getAllPostsId and use it to return paths which contains the array of known paths which include the params defined by [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,13 @@
 import Layout from "../../components/layout";
+import { getAllPostsId } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostsId();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default function Post() {
   return <Layout>...</Layout>;
