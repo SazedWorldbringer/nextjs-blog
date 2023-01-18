542a964533cf18e2dda871afeff2afa3622e12ec
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-18 22:48:13 +0530

    feat: update index.js to typescript in index.tsx, update blog about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import Head from "next/head";
-import Link from "next/link";
-import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -34,15 +34,7 @@ export default function Home({ allPostsData }) {
           profile.
         </p>
         <p>
-          I finally got to creating a blog following the official{" "}
-          <a
-            href="https://nextjs.org/learn/basics/create-nextjs-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Next.js tutorial
-          </a>
-          . Read about my learning, stuff I read and maybe some recipes.
+          Read about my learnings, stuff I read, and maybe some recipes, I don't know.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -62,3 +54,13 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
