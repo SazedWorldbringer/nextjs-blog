85f549610ec5b1cd18047720f973885d28736f6f
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-20 07:55:01 +0530

    feat: write new methods in lib/posts to getch journal entries, dynamic routing for journal entries

--- lib/posts.ts
+++ lib/posts.ts
@@ -5,6 +5,37 @@ import { remark } from "remark";
 import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
+const journalDirectory = path.join(process.cwd(), "journal");
+
+export function getSortedJournalEntriesData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(journalDirectory);
+  const allJournalEntriesData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(journalDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string }),
+    };
+  });
+  // Sort posts by date
+  return allJournalEntriesData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+}
 
 export function getSortedPostsData() {
   // Get file names under /posts
@@ -62,8 +93,41 @@ export function getAllPostsId() {
   });
 }
 
+export function getAllJournalEntriesId() {
+  const fileNames = fs.readdirSync(journalDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
 export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use grey-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into html string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with id
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string, title: string }),
+  };
+}
+
+export async function getJournalEntryData(id: string) {
+  const fullPath = path.join(journalDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   // Use grey-matter to parse the post metadata section
